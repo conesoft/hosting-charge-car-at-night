75a4088fb8fdc889c8e56fe4f4772e2e8b04cb8c
David Spörri <davepermen@hotmail.com>
2021-08-02 23:03:51 +0200

    from 0 to 100% or something..

---
+++ src/ECarUpHttpClient.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace EvState.HttpClients
+{
+    public class ECarUpHttpClient
+    {
+        private readonly HttpClient httpClient;
+        private readonly string id;
+
+        public ECarUpHttpClient(HttpClient httpClient, IConfiguration configuration)
+        {
+            var username = configuration["ecarup:username"];
+            var password = configuration["ecarup:password"];
+            var uri = configuration["ecarup:uri"];
+            var id = configuration["ecarup:id"];
+
+            this.httpClient = httpClient;
+            this.id = id;
+
+            httpClient.BaseAddress = new Uri(uri);
+            httpClient.DefaultRequestHeaders.Add("Authorization", "Basic " + Convert.ToBase64String(Encoding.UTF8.GetBytes(username + ":" + password)));
+        }
+
+        public async Task StartCharging(TimeSpan during)
+        {
+            var seconds = (int)during.TotalSeconds;
+            await httpClient.PostAsync("api/ActivateStation/" + id + "?seconds=" + seconds, null);
+        }
+
+        public async Task StopCharging()
+        {
+            await httpClient.PostAsync("api/DeactivateStation/" + id, null);
+        }
+
+        public async Task<ECarUp.ChargingState[]> State()
+        {
+            var response = await httpClient.GetAsync("api/ActiveStations");
+            var content = await response.Content.ReadAsStreamAsync();
+            return await JsonSerializer.DeserializeAsync<ECarUp.ChargingState[]>(content);
+        }
+
+        public async Task<ECarUp.History[]> GetHistory()
+        {
+            var response = await httpClient.GetAsync("api/DriverHistory?startDate=2015-12-28T15%3A12%3A35.023Z");
+            var content = await response.Content.ReadAsStreamAsync();
+            return await JsonSerializer.DeserializeAsync<ECarUp.History[]>(content);
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,40 @@
+﻿using EvState.HttpClients;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Conesoft.Services.ChargeCarAtNight
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder().AddJsonFile(Hosting.Host.GlobalSettings.Path).Build();
+            var client = new ECarUpHttpClient(new HttpClient(), configuration);
+
+            var lastRun = DateTime.MinValue;
+            var runAt = TimeSpan.FromHours(22);
+            var runFor = TimeSpan.FromHours(5);
+
+            while (true)
+            {
+                await Task.Delay(1000 - DateTime.Now.Millisecond);
+
+                Console.WriteLine($"tick @ {DateTime.Now}");
+
+                if (lastRun < DateTime.Today && (DateTime.Now - DateTime.Today) > runAt)
+                {
+                    lastRun = DateTime.Today;
+
+                    if((await client.State()).Length == 0)
+                    {
+                        await client.StartCharging(runFor);
+
+                        Console.WriteLine($"started charging for {DateTime.Today}");
+                    }
+                }
+            }
+        }
+    }
+}
