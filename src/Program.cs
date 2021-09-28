5536fd12886f3f295e32afd9def0df04fe0a7d25
David Spörri <davepermen@hotmail.com>
2021-09-28 23:30:50 +0200

    updated with net6

--- src/Program.cs
+++ src/Program.cs
@@ -1,40 +1,52 @@
 ﻿using EvState.HttpClients;
-using Microsoft.Extensions.Configuration;
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Extensions;
 
-namespace Conesoft.Services.ChargeCarAtNight
+var configuration = new ConfigurationBuilder().AddJsonFile(Conesoft.Hosting.Host.GlobalSettings.Path).Build();
+var client = new ECarUpHttpClient(new HttpClient(), configuration);
+
+var runAt = TimeSpan.FromHours(22);
+var runFor = TimeSpan.FromHours(5);
+
+
+while (true)
 {
-    class Program
+    var start = (int)Math.Floor(runAt.TotalMilliseconds - (DateTime.Now - DateTime.Today).TotalMilliseconds);
+    var end = (int)Math.Floor((runAt + runFor).TotalMilliseconds - (DateTime.Now - DateTime.Today).TotalMilliseconds);
+
+    if (start <= 0 && end > 0)
     {
-        static async Task Main(string[] args)
-        {
-            var configuration = new ConfigurationBuilder().AddJsonFile(Hosting.Host.GlobalSettings.Path).Build();
-            var client = new ECarUpHttpClient(new HttpClient(), configuration);
-
-            var lastRun = DateTime.MinValue;
-            var runAt = TimeSpan.FromHours(22);
-            var runFor = TimeSpan.FromHours(5);
-
-            while (true)
-            {
-                await Task.Delay(1000 - DateTime.Now.Millisecond);
-
-                Console.WriteLine($"tick @ {DateTime.Now}");
-
-                if (lastRun < DateTime.Today && (DateTime.Now - DateTime.Today) > runAt)
-                {
-                    lastRun = DateTime.Today;
-
-                    if((await client.State()).Length == 0)
-                    {
-                        await client.StartCharging(runFor);
-
-                        Console.WriteLine($"started charging for {DateTime.Today}");
-                    }
-                }
-            }
-        }
+        await Run();
     }
+
+    await Task.Delay((start + 86400000) % 86400000);
 }
+
+async Task Run()
+{
+    if ((await client.State()).Length == 0)
+    {
+        await client.StartCharging(runFor);
+
+        var message = new
+        {
+            title = $"Charging started",
+            message = $"Charging at Plattenstrasse 5 in Burg started",
+            image_url = $"https://i.imgur.com/OrOZqXt.jpg",
+            type = "Server"
+        };
+
+        await new HttpClient().GetAsync($@"https://wirepusher.com/send?id=mpgpt&title={message.title}&message={message.message}&type={message.type}&image_url={message.image_url}");
+    }
+    else
+    {
+        var message = new
+        {
+            title = $"Charging in progress",
+            message = $"Charging at Plattenstrasse 5 in Burg is in progress",
+            image_url = $"https://i.imgur.com/xP60kt7.png",
+            type = "Server"
+        };
+
+        await new HttpClient().GetAsync($@"https://wirepusher.com/send?id=mpgpt&title={message.title}&message={message.message}&type={message.type}&image_url={message.image_url}");
+    }
+};
